eced6283e8bc044e21b5318c8ff71645a5603851
Dave Snyder <daverted@gmail.com>
2020-04-06 19:33:59 -0600

    remove URL validation, format code

--- src/main/java/com/overops/plugins/bamboo/servlet/AdminServlet.java
+++ src/main/java/com/overops/plugins/bamboo/servlet/AdminServlet.java
@@ -1,7 +1,5 @@
 package com.overops.plugins.bamboo.servlet;
 
-import com.atlassian.sal.api.transaction.TransactionCallback;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,30 +10,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.atlassian.templaterenderer.TemplateRenderer;
 import com.atlassian.sal.api.pluginsettings.PluginSettings;
 import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
-import com.atlassian.templaterenderer.TemplateRenderer;
 import com.atlassian.sal.api.transaction.TransactionTemplate;
+import com.atlassian.sal.api.transaction.TransactionCallback;
 
 import com.overops.plugins.bamboo.model.TestServiceResponse;
 import com.overops.plugins.bamboo.service.SettingService;
 import com.overops.plugins.bamboo.service.impl.SettingServiceImpl;
 import com.overops.plugins.bamboo.configuration.Const;
+
 import org.springframework.util.StringUtils;
 
 public class AdminServlet extends HttpServlet {
-public static final String OVEROPS_ADMIN_VM = "overopsadmin.vm";
+    public static final String OVEROPS_ADMIN_VM = "overopsadmin.vm";
+    private static final long serialVersionUID = 1L;
+
     private final TransactionTemplate transactionTemplate;
     private final PluginSettingsFactory pluginSettingsFactory;
-    private static final long serialVersionUID = 1L;
-    private final String URL_REGEX = "^https://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
-
     private final TemplateRenderer renderer;
+
     private SettingService settingService;
 
-
-    public AdminServlet(PluginSettingsFactory pluginSettingsFactory, TemplateRenderer renderer,
-                        TransactionTemplate transactionTemplate) {
+    public AdminServlet(PluginSettingsFactory pluginSettingsFactory, TemplateRenderer renderer, TransactionTemplate transactionTemplate) {
         this.pluginSettingsFactory = pluginSettingsFactory;
         this.renderer = renderer;
         this.transactionTemplate = transactionTemplate;
@@ -44,23 +42,32 @@ public static final String OVEROPS_ADMIN_VM = "overopsadmin.vm";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Map<String, Object> context = new HashMap<String, Object>();
-        resp.setContentType("text/html;charset=utf-8");
 
+        // get/set global plugin settings
         PluginSettings pluginSettings = pluginSettingsFactory.createGlobalSettings();
-        String url = Optional.ofNullable(pluginSettings.get(Const.API_URL)).map(u -> (String) u).filter(StringUtils::hasLength).map(String::trim)
-                .orElse(Const.DEFAULT_URL);
-        context.put(Const.API_URL, url.trim());
 
-        String env = Optional.ofNullable(pluginSettings.get(Const.API_ENV)).map(e -> (String) e).map(String::trim).orElse("");
+        // velocity template context
+        Map<String, Object> context = new HashMap<String, Object>();
 
-        context.put(Const.API_ENV, env);
+        // if blank, set default value
+        String url = Optional.ofNullable(pluginSettings.get(Const.GLOBAL_API_URL)).map(u -> (String) u).filter(StringUtils::hasLength).map(String::trim)
+                .orElse(Const.DEFAULT_API_URL);
 
-        String token = Optional.ofNullable(pluginSettings.get(Const.API_TOKEN)).map(t -> (String) t).map(String::trim).orElse("");
-        context.put(Const.API_TOKEN, token);
+        context.put("url", url);
+
+        String env = Optional.ofNullable(pluginSettings.get(Const.GLOBAL_API_ENV_ID)).map(e -> (String) e).map(String::trim).orElse("");
+
+        context.put(Const.GLOBAL_API_ENV_ID, env);
+
+        String token = Optional.ofNullable(pluginSettings.get(Const.GLOBAL_API_TOKEN)).map(t -> (String) t).map(String::trim).orElse("");
+
+        context.put(Const.GLOBAL_API_TOKEN, token);
+
         context.put(Const.ERROR, "");
         context.put(Const.INFO, "");
         context.put("newEventsGate", false);
+
+        resp.setContentType("text/html;charset=utf-8");
         renderer.render(OVEROPS_ADMIN_VM, context, resp.getWriter());
     }
 
@@ -69,21 +76,13 @@ public static final String OVEROPS_ADMIN_VM = "overopsadmin.vm";
         Map<String, Object> context = new HashMap<String, Object>();
         resp.setContentType("text/html;charset=utf-8");
 
-        String url = req.getParameter(Const.API_URL).trim().replaceAll("/+$", "");
-        String env = req.getParameter(Const.API_ENV).trim();
-        String token = req.getParameter(Const.API_TOKEN).trim();
+        String url = req.getParameter(Const.GLOBAL_API_URL).trim();
+        String env = req.getParameter(Const.GLOBAL_API_ENV_ID).trim();
+        String token = req.getParameter(Const.GLOBAL_API_TOKEN).trim();
 
-        context.put(Const.API_URL, url);
-        context.put(Const.API_ENV, env);
-        context.put(Const.API_TOKEN, token);
-
-        if (!url.matches(URL_REGEX)) {
-            context.put(Const.ERROR, "OverOps url is not saved! Invalid format!");
-            context.put(Const.INFO, "");
-
-            renderer.render(OVEROPS_ADMIN_VM, context, resp.getWriter());
-            return;
-        }
+        context.put(Const.GLOBAL_API_URL, url);
+        context.put(Const.GLOBAL_API_ENV_ID, env);
+        context.put(Const.GLOBAL_API_TOKEN, token);
 
         try {
             TestServiceResponse response = settingService.testConnection(url, env, token);
@@ -91,9 +90,9 @@ public static final String OVEROPS_ADMIN_VM = "overopsadmin.vm";
             if (response.isStatus()) {
                 transactionTemplate.execute((TransactionCallback) () -> {
                     PluginSettings pluginSettings = pluginSettingsFactory.createGlobalSettings();
-                    pluginSettings.put(Const.API_URL, url);
-                    pluginSettings.put(Const.API_ENV, env);
-                    pluginSettings.put(Const.API_TOKEN, token);
+                    pluginSettings.put(Const.GLOBAL_API_URL, url);
+                    pluginSettings.put(Const.GLOBAL_API_ENV_ID, env);
+                    pluginSettings.put(Const.GLOBAL_API_TOKEN, token);
                     return pluginSettings;
                 });
                 context.put(Const.INFO, "OverOps settings are updated. Check that jobs are configured properly");
